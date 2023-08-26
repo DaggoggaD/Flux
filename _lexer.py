4a5559f124a08757fa701516ae9e3724f37c4c5c
DaggoggaD <95711168+DaggoggaD@users.noreply.github.com>
2023-08-26 15:16:09 +0200

    Updated Lexer.
    Lexer is now much simpler. Still needs some tweaks but the improvement is evident.
    Most of the most important bugs in the old lexer are now solved, as it is easier to debg now.

---
+++ _lexer.py
@@ -0,0 +1,330 @@
+DIGITS = '0123456789'
+after_kw = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
+
+###tokens###
+T_INT = "INT"
+T_FLOAT = "FLOAT"
+T_STRING = "STRING"
+T_ARRAY = "ARRAY"
+T_PLUS = "PLUS"
+T_MINUS = "MINUS"
+T_MUL = "MUL"
+T_DIV = "DIV"
+T_POW = "POW"
+T_SQRT = "SQRT"
+T_GRT = "GRT"
+T_LST = "LST"
+T_GOE = "GOE"
+T_LOE = "LOE"
+T_LPAR = "LPAR"
+T_RPAR = "RPAR"
+T_LGPAR = "LGPAR"
+T_RGPAR = "RGPAR"
+T_EQUAL = "EQUAL"
+T_NOTEQUAL = "NOTEQUAL"
+T_KEYWORD = "KW"
+T_IDENTIFIER = "ID"
+T_NEWLINE = "NEWLINE"
+T_EOF = "EOF"
+T_ERROR = "ERROR"
+T_TRUE = "TRUE"
+T_FALSE = "FALSE"
+###tokens###
+
+###keywords, Operations###
+keywords = [
+    "if",
+    "else",
+    "for",
+    "while",
+    "func",
+    "store",
+    "print",
+    "func",
+    "in",
+    "GOE",
+    "LOE",
+    "NOE",
+    "getAV",
+    "setAV"
+]
+
+operations = [
+    "+",
+    "-",
+    "*",
+    "/",
+    "**",
+    "#",
+    "=",
+    ">",
+    "<",
+    "{",
+    "}",
+    "(",
+    ")",
+    ";",
+]
+
+operations_dict = {
+    "+" : T_PLUS,
+    "-" : T_MINUS,
+    "*" : T_MUL,
+    "/" : T_DIV,
+    "**": T_POW,
+    "#" : T_SQRT,
+    "=" : T_EQUAL,
+    ">" : T_GRT,
+    "<" : T_LST,
+    "{" : T_LGPAR,
+    "}" : T_RGPAR,
+    "(" : T_LPAR,
+    ")" : T_RPAR,
+    ";" : "SC",
+}
+###keywords###
+
+def is_int(n):
+    try:
+        float_n = float(n)
+        int_n = int(float_n)
+    except ValueError:
+        return False
+    else:
+        return float_n == int_n
+
+def is_float(n):
+    try:
+        float_n = float(n)
+    except ValueError:
+        return False
+    else:
+        return True
+
+class Token:
+    def __init__(self, T_TYPE, value=None, arrLen=None):
+        self.T_TYPE = T_TYPE
+        self.value = value
+        self.arrLen = arrLen
+
+    def __repr__(self):
+        if self.value:
+            if self.arrLen: return f"{self.T_TYPE}: {self.value}"
+            return f"{self.T_TYPE}: {self.value}"
+        else:
+            if self.value == 0:
+                return f"{self.T_TYPE}: {self.value}"
+            return f"{self.T_TYPE}"
+
+    def matches(self, type_, value_):
+        return self.T_TYPE == type_ and self.value == value_
+
+class Lexer:
+    def __init__(self, filename):
+        self.lines = self.initialize(filename)
+        self.currline = 0
+        self.linepos = -1
+        self.not_known_str = ""
+        self.next_char = ""
+        self.file_ended = False
+        self.tokens = None
+        self.found = False
+        self.advance()
+
+    def advance(self):
+        if self.currline < len(self.lines):
+            self.linepos+=1
+            if self.linepos < len(self.lines[self.currline]):
+                self.not_known_str+=self.lines[self.currline][self.linepos]
+                if self.linepos+1 < len(self.lines[self.currline]):
+                    self.next_char=self.lines[self.currline][self.linepos+1]
+                elif self.currline+1<len(self.lines):
+                    self.next_char=self.lines[self.currline+1][0]
+            else:
+                self.linepos=-1
+                self.currline+=1
+        else:
+            self.file_ended=True
+
+    def check_kw(self):
+        if self.found:
+            return None
+        if self.not_known_str in keywords:
+            if self.next_char in operations or self.next_char == " ":
+                ret_str = self.not_known_str
+                self.not_known_str = ""
+                self.found = True
+                return Token(T_KEYWORD, ret_str)
+        return None
+
+    def check_id(self):
+        if self.found:
+            return None
+        if (self.next_char in operations or self.next_char == " ") and self.not_known_str!="":
+            ret_str = self.not_known_str
+            self.not_known_str = ""
+            self.found = True
+            return Token(T_IDENTIFIER, ret_str)
+
+    def check_op(self):
+        if self.found:
+            return None
+        if self.not_known_str in operations:
+            ret_str = self.not_known_str
+            self.not_known_str = ""
+            self.found = True
+            needed_T = operations_dict[ret_str]
+            if needed_T!="SC" and needed_T!=T_LGPAR and needed_T!=T_RGPAR:
+                return [Token(needed_T)]
+            elif needed_T=="SC":
+                return [Token(T_KEYWORD, ";"), Token(T_NEWLINE)]
+            else:
+                return [Token(needed_T), Token(T_NEWLINE)]
+
+    def check_num(self):
+        num = self.not_known_str
+        if self.found:
+            return None
+        while is_int(num+self.next_char) or is_float(num+self.next_char):
+            num = self.not_known_str
+            self.advance()
+        num=self.not_known_str
+
+        if is_int(num) or is_float(num):
+            self.not_known_str = ""
+            try:
+                self.found=True
+                return Token(T_INT, int(num))
+            except:
+                self.found=True
+                return Token(T_FLOAT, float(num))
+        else:
+            return None
+
+    #is used in check_arr for multidimensional arrays
+    def find_inside_arr(self, arrvals, arrval, i):
+        new_arr = [arrval[1:]]
+        i += 1
+        opened = 1
+        while opened != 0 and i < len(arrvals):
+            arrval = arrvals[i]
+            if arrval == "]":
+                opened -= 1
+            elif arrval[0] == "[":
+                ret_arr, i = self.find_inside_arr(arrvals,arrval,i)
+                new_arr.append(ret_arr)
+            else:
+                new_arr.append(arrval)
+            i += 1
+        final_ret_arr = []
+        for val in new_arr:
+            if type(val)!=Token:
+                if is_int(val):
+                    final_ret_arr.append(int(val))
+                elif is_float(val):
+                    final_ret_arr.append(float(val))
+                else:
+                    final_ret_arr.append(str(val).strip('"'))
+            else:
+                final_ret_arr.append(val)
+        return Token(T_ARRAY, final_ret_arr, len(final_ret_arr)), i-1
+
+    #checks first part of an array
+    def check_arr(self):
+        arr = self.not_known_str
+        lpar=0
+        if self.found:
+            return None
+        # Tries to create an array
+        if arr == "[":
+            lpar+=1
+        while lpar != 0:
+            if self.next_char=="[":
+                lpar+=1
+            elif self.next_char == "]":
+                lpar-=1
+            self.advance( )
+            arr = self.not_known_str
+        arrvals = []
+        #separates values with "," or " "
+        if arr != "":
+            if arr[0]=="[" and arr[-1]=="]":
+                arr=arr[1:]
+                val = ""
+                for char in arr:
+                    if char!=",":
+                        val+=char
+                    else:
+                        arrvals.append(val)
+                        val=""
+
+        #checks if each value is a string, an int, a float or another array.
+        final_ret_arr = []
+        i=0
+        while i < len(arrvals):
+            arrval = arrvals[i]
+            if is_int(arrval):
+                final_ret_arr.append(int(arrval))
+            elif is_float(arrval):
+                final_ret_arr.append(float(arrval))
+            elif arrval.find("[")!=-1:
+                #if theres an open "[", checks for the possibility of a multi-dimensional array
+                new_arr, i = self.find_inside_arr(arrvals,arrval,i)
+                final_ret_arr.append(new_arr)
+            else:
+                final_ret_arr.append(str(arrval).strip('"'))
+            i+=1
+        if final_ret_arr!=[]:
+            self.found=True
+            self.not_known_str=""
+            return Token(T_ARRAY, final_ret_arr, len(final_ret_arr))
+
+    def check_string(self):
+        if self.found:
+            return None
+        if len(self.not_known_str)>=2:
+            if self.not_known_str[0] == '"' and self.not_known_str[-1]=='"':
+                self.found=True
+                found = self.not_known_str
+                self.not_known_str=""
+                return Token(T_STRING, found.strip('"'))
+
+    def RUN(self):
+        tokens = []
+        while self.file_ended==False:
+            self.not_known_str = self.not_known_str.strip()
+            self.found=False
+            _kw = self.check_kw()
+            _ops = self.check_op()
+            _num = self.check_num()
+            _arr = self.check_arr()
+            _str = self.check_string()
+            _id = self.check_id()
+            #done so to be simple to understand
+            if _kw:
+                tokens.append(_kw)
+            if _ops:
+                for op in _ops:
+                    tokens.append(op)
+            if _num:
+                tokens.append(_num)
+            if _arr:
+                tokens.append(_arr)
+            if _str:
+                tokens.append(_str)
+            if _id:
+                tokens.append(_id)
+
+            self.advance()
+        tokens.append(Token(T_EOF))
+        for tok in tokens:
+            if tok==None:
+                tokens.remove(tok)
+        self.tokens = tokens
+
+    def initialize(self, filename):
+        f = open(filename)
+        lines = []
+        for line in f:
+            lines.append(line)
+        return lines
---
+++ _parser.py
@@ -0,0 +1,332 @@
+#
+from _lexer import *
+
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+    def __repr__(self):
+        return f"{self.tok}"
+
+
+class BinOP:
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def __repr__(self):
+        return f"({self.left},{self.op},{self.right})"
+
+
+class VarAssignNode:
+    def __init__(self, var_name, expr_value):
+        self.var_name = var_name
+        self.expr_value = expr_value
+
+    def __repr__(self):
+        return f"(assign: {self.var_name}, EQUALS, {self.expr_value})"
+
+
+class IfStatement:
+    def __init__(self, compexpr, expression):
+        self.compexpr = compexpr
+        self.expression = expression
+
+    def __repr__(self):
+        return f"(if {self.compexpr}: {self.expression})"
+
+
+class ElseStatement:
+    def __init__(self, expression):
+        self.expression = expression
+
+    def __repr__(self):
+        return f"(else: {self.expression})"
+
+
+class WhileStatement:
+    def __init__(self, compexpr, expression):
+        self.compexpr = compexpr
+        self.expression = expression
+
+    def __repr__(self):
+        return f"(while {self.compexpr}: {self.expression})"
+
+
+class PrintStatement:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f"(print: {self.value})"
+
+
+class FuncStatement:
+    def __init__(self, funcnametok, arguments, expression):
+        self.funcnametok = funcnametok
+        self.arguments = arguments
+        self.expression = expression
+
+    def __repr__(self):
+        return f"(func arguments{self.arguments}: {self.expression})"
+
+
+class GetAVStatement:
+    def __init__(self, array, location):
+        self.array = array
+        self.location = location
+
+    def __repr__(self):
+        return f"(getAV: {self.array}[{self.location}])"
+
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.currtok = None
+        self.tokidx = -1
+        self.advance( )
+        self.recentlyEndedIf = False
+
+    def advance(self):
+        self.tokidx += 1
+        if self.tokidx < len(self.tokens):
+            self.currtok = self.tokens[self.tokidx]
+
+    def factor(self):
+        tok = self.currtok
+        try:
+            if tok.T_TYPE in (T_INT, T_FLOAT, T_IDENTIFIER, T_STRING, T_ARRAY):
+                self.advance( )
+                return NumberNode(tok)
+        except:
+            return NumberNode(tok)
+
+    def term(self):
+        left = self.factor( )
+        while self.currtok.T_TYPE in (T_MUL, T_DIV):
+            op_token = self.currtok
+            self.advance( )
+            right = self.factor( )
+            left = BinOP(left, op_token, right)
+
+        return left
+
+    def leftest_expression_modifier(self, expr, replace_expr):
+        def go_left(curr_expr):
+            if curr_expr.left != None: return curr_expr.left
+
+        left_depth = 0
+        parts = []
+        curr_expr = expr
+        ending_expr = BinOP(None, None, None)
+        while curr_expr.left != None:
+            left_depth += 1
+            curr_expr = go_left(curr_expr)
+
+        i = 0
+        while i < left_depth:
+            for n in range(i):
+                curr_expr = go_left(expr)
+            op = curr_expr.op
+            right = curr_expr.right
+            if ending_expr.left == None:
+                ending_expr = BinOP(replace_expr, op, right)
+            else:
+                ending_expr = BinOP(ending_expr, op, right)
+            i += 1
+        if ending_expr.left == None:
+            return BinOP(replace_expr, expr.op, expr.right)
+        ending_expr = BinOP(ending_expr, expr.op, expr.right)
+        return ending_expr
+
+    def expr(self):
+        # VARIABLE ASSIGNMENT
+        if self.currtok.matches(T_KEYWORD, "store"):
+            self.advance( )
+            if self.currtok.T_TYPE != T_IDENTIFIER:
+                print("Expected identifier after store")
+                return Token(T_ERROR)
+            var_name = self.currtok
+            self.advance( )
+            if self.currtok.T_TYPE != T_EQUAL:
+                print("Expected equals after variable declaration")
+                return Token(T_ERROR)
+            self.advance( )
+            expr = self.expr( )
+            oldexpr = expr
+            if type(expr) == GetAVStatement:
+                expr = self.expr( )
+                if expr != None:
+                    expr = self.leftest_expression_modifier(expr, oldexpr)
+                else:
+                    expr = oldexpr
+
+                while self.currtok.T_TYPE in (T_PLUS, T_MINUS, T_LST, T_GRT, T_LOE, T_GOE, T_EQUAL, T_NOTEQUAL):
+                    expr = self.expr( )
+
+            self.advance( )
+            if self.currtok.T_TYPE == T_NEWLINE or self.currtok.T_TYPE == T_RPAR:
+                self.recentlyEndedIf = False
+                return VarAssignNode(var_name, expr)
+            else:
+                print("missing ';'")
+                return Token(T_ERROR)
+        # IF STATEMENT
+        if self.currtok.matches(T_KEYWORD, "if"):
+            self.advance( )
+            if self.currtok.T_TYPE != T_LPAR:
+                print("Required '(' after if statement")
+                return Token(T_ERROR)
+            self.advance( )
+            compare_expression = self.expr( )
+            if self.currtok.T_TYPE != T_RPAR:
+                print("Required ')' after if statement")
+                return Token(T_ERROR)
+            self.advance( )
+            if self.currtok.T_TYPE != T_LGPAR:
+                print("Required '{' at if opening")
+                return Token(T_ERROR)
+            self.advance( )
+            # skip NEWLINE
+            if self.currtok.T_TYPE == T_NEWLINE:
+                self.advance( )
+            expressions = []
+            while self.currtok.T_TYPE != T_RGPAR:
+                expressions.append(self.expr( ))
+                self.advance( )
+            self.advance( )
+            self.recentlyEndedIf = True
+            return IfStatement(compare_expression, expressions)
+        # ELSE STATEMENT
+        if self.currtok.matches(T_KEYWORD, "else"):
+            if self.recentlyEndedIf:
+                self.advance( )
+                if self.currtok.T_TYPE != T_LGPAR:
+                    print("Required '{' after else statement")
+                    return Token(T_ERROR)
+                self.advance( )
+                # skip NEWLINE
+                if self.currtok.T_TYPE == T_NEWLINE:
+                    self.advance( )
+                expressions = []
+                while self.currtok.T_TYPE != T_RGPAR:
+                    expressions.append(self.expr( ))
+                    self.advance( )
+                self.advance( )
+                self.recentlyEndedIf = False
+                return ElseStatement(expressions)
+            else:
+                print("expected if before else (try to move the else statement closer to the desired if")
+                return Token(T_ERROR)
+        # WHILE STATEMENT
+        if self.currtok.matches(T_KEYWORD, "while"):
+            self.advance( )
+            if self.currtok.T_TYPE != T_LPAR:
+                print("Required '(' after if statement")
+                return Token(T_ERROR)
+            self.advance( )
+            compare_expression = self.expr( )
+            if self.currtok.T_TYPE != T_RPAR:
+                print("Required ')' after if statement")
+                return Token(T_ERROR)
+            self.advance( )
+            if self.currtok.T_TYPE != T_LGPAR:
+                print("Required '{' at if opening")
+                return Token(T_ERROR)
+            self.advance( )
+            # skip NEWLINE
+            if self.currtok.T_TYPE == T_NEWLINE:
+                self.advance( )
+            expressions = []
+            while self.currtok.T_TYPE != T_RGPAR:
+                expressions.append(self.expr( ))
+                self.advance( )
+            self.advance( )
+            return WhileStatement(compare_expression, expressions)
+        # PRINT STATEMENT
+        if self.currtok.matches(T_KEYWORD, "print"):
+            self.advance( )
+            if self.currtok.T_TYPE != T_LPAR:
+                print("Required '(' after print statement")
+                return Token(T_ERROR)
+            self.advance( )
+            print_value = self.expr( )
+            if self.currtok.T_TYPE != T_RPAR:
+                print("Required ')' after if statement")
+                return Token(T_ERROR)
+            self.advance( )
+            return PrintStatement(print_value)
+        # getAV STATEMENT
+        if self.currtok.matches(T_KEYWORD, "getAV"):
+            self.advance( )
+            if self.currtok.T_TYPE != T_LPAR:
+                print("Required '(' after getAV statement")
+                return Token(T_ERROR)
+            self.advance( )
+            arr_name = self.currtok
+            """changed"""
+            if arr_name.value == "getAV":
+                arr_name = self.expr()
+            else:
+                self.advance( )
+            var_location = self.currtok
+            self.advance( )
+            if self.currtok.T_TYPE != T_RPAR:
+                print("Required ')' after if statement")
+                return Token(T_ERROR)
+            self.advance( )
+            return GetAVStatement(arr_name, var_location)
+        # FUNCTION STATEMENT#
+        if self.currtok.matches(T_KEYWORD, "func"):
+            self.advance( )
+            if self.currtok.T_TYPE != T_IDENTIFIER:
+                print("Required name of function")
+                return Token(T_ERROR)
+            func_name_token = self.currtok
+            self.advance( )
+            if self.currtok.T_TYPE != T_LPAR:
+                print("Required '(' after if statement")
+                return Token(T_ERROR)
+            self.advance( )
+            arguments = []
+            while self.currtok.T_TYPE != T_RPAR:
+                arguments.append(self.expr( ))
+                self.advance( )
+            self.advance( )
+            if self.currtok.T_TYPE != T_LGPAR:
+                print("Required '{' at if opening")
+                return Token(T_ERROR)
+            self.advance( )
+            # skip NEWLINE
+            if self.currtok.T_TYPE == T_NEWLINE:
+                self.advance( )
+            expressions = []
+            while self.currtok.T_TYPE != T_RGPAR:
+                expressions.append(self.expr( ))
+                self.advance( )
+            self.advance( )
+            self.recentlyEndedIf = False
+            """NEEDS TO COMPLETE ARGUMENTS (, DOESNT WORK"""
+            return FuncStatement(func_name_token, arguments, expressions)
+        # MATH OPERATIONS
+        left = self.term( )
+        while self.currtok.T_TYPE in (T_PLUS, T_MINUS, T_LST, T_GRT, T_LOE, T_GOE, T_EQUAL, T_NOTEQUAL):
+            op_token = self.currtok
+            self.advance( )
+            right = None
+            if self.currtok.matches(T_KEYWORD, "getAV"):
+                right = self.expr( )
+            else:
+                right = self.term( )
+            left = BinOP(left, op_token, right)
+        return left
+
+    def run(self):
+        res_expr = []
+        while self.currtok.T_TYPE != T_EOF:
+            res = self.expr( )
+            if res != None: res_expr.append(res)
+            self.advance( )
+        return res_expr
